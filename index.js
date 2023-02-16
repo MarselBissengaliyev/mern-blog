e57510faa9dbb459f641aae5a0957e730535ed3a
Marsel <marselbisengaliev1@gmail.com>
2023-02-16 21:56:20 +0500

    add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+    .connect('mongodb+srv://admin:123456w@cluster0.adltjnx.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err))
+
 
 const app = express();
 
