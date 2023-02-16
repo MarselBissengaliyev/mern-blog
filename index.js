f2da0dc2b9e7658775e329e851a596d59bbc5ed4
Marsel <marselbisengaliev1@gmail.com>
2023-02-16 21:54:25 +0500

    add jwttoken

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Вася Пупкин'
+    }, 'secret123');
+    res.json({
+        success: true,
+        token
+    });
 });
 
 app.listen(4444, (err) => {
@@ -12,4 +28,4 @@ app.listen(4444, (err) => {
     }
 
     console.log('Server OK');
-})
+});
