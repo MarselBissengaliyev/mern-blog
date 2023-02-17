436a18a735b1340bf00c04a68016c4b3acd6141b
Marsel <marselbisengaliev1@gmail.com>
2023-02-17 19:14:39 +0500

    create user model & install bcrypt & hash password & add register validation message

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 
 import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js';
 
 dotenv.config()
 
@@ -17,15 +20,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     } 
-    
-    res.json({
-        success: true,
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash
     });
+
+    const user = await doc.save();
+    
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
