d02d3fc6c5fe95588625b2b9807c6dec97b1e725
Marsel <marselbisengaliev1@gmail.com>
2023-02-18 21:20:46 +0500

    create functionality for posts (CRUD)

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validation.js';
 
 import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 dotenv.config()
 
@@ -19,11 +20,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login)
-
+app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/auth/me', checkAuth, UserController.getMe)
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
