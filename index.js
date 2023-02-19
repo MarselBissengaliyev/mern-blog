559110d91f18793990102b987028f86ef2f8152b
Marsel <marselbisengaliev1@gmail.com>
2023-02-19 13:20:39 +0500

    add the functionality of uploading a picture to the server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import multer from 'multer';
+
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
@@ -18,11 +20,29 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
