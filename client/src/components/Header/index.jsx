ab782e495de4eaea2b5ef6d21f1dea4d53262449
Marsel <marselbisengaliev1@gmail.com>
2023-02-20 16:33:52 +0500

    frontend.provide redux toolkit

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>ARCHAKOV BLOG</div>
+            <div>MARSEL BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +17,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter> 
     </ThemeProvider>
   </React.StrictMode>
